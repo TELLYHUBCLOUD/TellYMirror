7c8de7b85df030f33db35b0f755a385a85e5203c
TELLYHUBCLOUD <tellyhubcloud@gmail.com>
2026-01-22 21:59:14 +0530

    Update config_manager.py

--- bot/core/config_manager.py
+++ bot/core/config_manager.py
@@ -235,8 +235,8 @@ class Config:
 
 
 class BinConfig:
-    ARIA2_NAME = "speeddemon"
-    QBIT_NAME = "torrentgod"
-    FFMPEG_NAME = "vidwarlock"
-    RCLONE_NAME = "cloudphantom"
-    SABNZBD_NAME = "newsslayer"
+    ARIA2_NAME = "aria2c"
+    QBIT_NAME = "qbittorrent-nox"
+    FFMPEG_NAME = "ffmpeg"
+    RCLONE_NAME = "rclone"
+    SABNZBD_NAME = "sabnzbdplus"
